cd41c0b4a6a341f4f0f4472a4f58abd5a511f8df
Juan Gutierrez <juanny.gee@gmail.com>
2015-06-19 17:34:07 -0400

    trim the fat of what the import visitor and walker classes are doing

--- module_dependency_hunter/ast_walker/walker.py
+++ module_dependency_hunter/ast_walker/walker.py
@@ -23,8 +23,7 @@ class ImportVisitor(object):
 
     def visitImport(self, node):
         self.accept_imports()
-        self.recent.extend((x[0], None, x[1] or x[0], node.lineno, 0)
-                           for x in node.names)
+        self.recent.extend((x[0], None, x[1] or x[0]) for x in node.names)
 
     def visitFrom(self, node):
         self.accept_imports()
@@ -34,26 +33,19 @@ class ImportVisitor(object):
         for name, as_ in node.names:
             if name == '*':
                 # We really don't know...
-                mod = (modname, None, None, node.lineno, node.level)
+                mod = (modname, None, None)
             else:
-                mod = (modname, name, as_ or name, node.lineno, node.level)
+                mod = (modname, name, as_ or name)
             self.recent.append(mod)
 
-    def default(self, node):
+    def default(self, _):
         """Needed for compiler.walk"""
-        pragma = None
-        if self.recent:
-            if isinstance(node, Discard):
-                children = node.getChildren()
-                if len(children) == 1 and isinstance(children[0], Const):
-                    const_node = children[0]
-                    pragma = const_node.value
-        self.accept_imports(pragma)
+        self.accept_imports()
 
-    def accept_imports(self, pragma=None):
+    def accept_imports(self):
         """accept imports"""
-        self.modules.extend((m, r, l, n, lvl, pragma)
-                            for (m, r, l, n, lvl) in self.recent)
+        self.modules.extend((m, r, l)
+                            for (m, r, l) in self.recent)
         self.recent = []
 
     def finalize(self):
